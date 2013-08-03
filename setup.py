f19471414a1517fad1bca015c9ba3aaa4e8e8fda
Mathew Odden <mrodden@us.ibm.com>
2013-08-02 23:21:32 -0500

    Add entry point scripts for CLI create/delete

--- setup.py
+++ setup.py
@@ -3,5 +3,11 @@ setup(
     name = "gitoriouslib",
     version = "0.1",
     packages = find_packages(),
-    install_requires = ['httplib2>=0.7.2']
+    install_requires = ['httplib2>=0.7.2'],
+    entry_points = {
+        'console_scripts': [
+            'glcreate = gitoriouslib.cmd:create_repo',
+            'gldelete = gitoriouslib.cmd:delete_repo',
+        ]
+    },
 )
